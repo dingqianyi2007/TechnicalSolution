3d46da1429640fe2f79bb8303dae7b7922a95261
MrDing <dingqianyi2007@163.com>
2019-06-05 00:12:34 +0800

    样式化组件和自定义主题

--- app/src/main/java/com/dqy/technicalsolution/chapter/layoutview/LayoutViewActivity.java
+++ app/src/main/java/com/dqy/technicalsolution/chapter/layoutview/LayoutViewActivity.java
@@ -1,8 +1,11 @@
 package com.dqy.technicalsolution.chapter.layoutview;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,5 +25,17 @@ public class LayoutViewActivity extends AppCompatActivity {
         lv_layout_view=findViewById(R.id.lv_layout_view);
         mAdapter = ArrayAdapter.createFromResource(mContext,R.array.layoutViewData_array,android.R.layout.simple_list_item_1);
         lv_layout_view.setAdapter(mAdapter);
+        lv_layout_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        startActivity(new Intent(LayoutViewActivity.this,StyledComponentsActivity.class));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 }
