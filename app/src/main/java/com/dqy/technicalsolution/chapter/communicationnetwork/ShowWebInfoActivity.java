6aea0ee0be0a1a136175ab28495b4fd7c4505fbb
MrDing <dingqianyi2007@163.com>
2019-06-04 22:48:07 +0800

    显示Web信息

--- app/src/main/java/com/dqy/technicalsolution/chapter/communicationnetwork/ShowWebInfoActivity.java
+++ app/src/main/java/com/dqy/technicalsolution/chapter/communicationnetwork/ShowWebInfoActivity.java
@@ -1,5 +1,7 @@
 package com.dqy.technicalsolution.chapter.communicationnetwork;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebResourceRequest;
@@ -19,7 +21,16 @@ public class ShowWebInfoActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+                if(url.startsWith("http:")||url.startsWith("https:")){
+                    return false;
+                }
+                try{
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                }catch (Exception e){
+                    return true;
+                }
+               view.loadUrl(url);
                 return true;
             }
         });
