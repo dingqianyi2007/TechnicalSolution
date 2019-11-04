035f60f13fbfddc766803c8e28a6525eb01a2c8c
MrDing <dingqianyi2007@163.com>
2019-11-04 09:45:36 +0800

    蓝牙

---
+++ app/src/main/java/com/dqy/technicalsolution/chapter/communicationnetwork/ExchangeActivity.java
@@ -0,0 +1,15 @@
+package com.dqy.technicalsolution.chapter.communicationnetwork;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dqy.technicalsolution.R;
+
+public class ExchangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange);
+    }
+}
